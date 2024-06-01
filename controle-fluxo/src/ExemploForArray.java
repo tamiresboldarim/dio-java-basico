908c19da6ba958febf1e40ed9cbbd85b45490dd6
tamiresboldarim <tamiresboldarim@hotmail.com>
2024-06-01 20:53:26 -0300

    Estruturas de Repetição

---
+++ controle-fluxo/src/ExemploForArray.java
@@ -0,0 +1,11 @@
+public class ExemploForArray {
+    public static void main(String[] args) {
+
+        //em arrays o índice inicia em ZERO
+        String alunos[] = { "FELIPE", "JONAS", "JULIA", "MARCOS" };
+
+	for (String aluno: alunos){
+		System.out.println("O aluno no aluno é: " + aluno);
+	}
+    }
+}
